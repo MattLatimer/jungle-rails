beba8ca3b9e54d78534c6cd2497ca01447616064
Matt Latimer <matt.latimer@gmail.com>
2017-10-13 13:27:58 -0700

    added TODO for validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,5 +6,5 @@ class Review < ActiveRecord::Base
   validates :product_id, presence: true, numericality: {only_integer: true}
   validates :user_id, presence: true, numericality: {only_integer: true}
   validates :rating, presence: true, numericality: {only_integer: true}, inclusion: {in: (1..5)}
-
+# TODO validate that product / user exists in database before save
 end
