e3347193145b3ec0346c6539639784137009a2ff
Matt Latimer <matt.latimer@gmail.com>
2017-10-13 15:57:59 -0700

    Added review controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @review = Review.new(review_params)
+    @product = Product.find(params[:product_id])
+    @review.product = @product
+    @review.user = current_user
+
+    if @review.save
+      redirect_to @product
+    else
+      render template: 'products/show'
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+end
