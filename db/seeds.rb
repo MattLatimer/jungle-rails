0bd88a7d01b68a3cd2123d4af106e0b0d3245e93
Matt Latimer <matt.latimer@gmail.com>
2017-10-13 14:20:21 -0700

    Added two users to db seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -132,6 +132,20 @@ cat3.products.create!({
   price: 2_483.75
 })
 
+puts "Adding a couple users..."
+
+User.create!({
+  name: 'Alice',
+  email: 'alice@email.com',
+  password_digest: 'not blank'
+})
+
+User.create!({
+  name: 'Bob',
+  email: 'bob@email.com',
+  password_digest: 'not empty'
+})
+
 puts "Making some reviews..."
 
 prod1 = Product.first
@@ -149,19 +163,19 @@ prod1.reviews.create!({
   rating: 5
 })
 
-prod1.reviews.create!({
+prod2.reviews.create!({
   user_id: 1,
   description: 'Meh',
   rating: 3
 })
 
-prod1.reviews.create!({
+prod2.reviews.create!({
   user_id: 2,
   description: 'Sort of good, sometimes.',
   rating: 4
 })
 
-prod1.reviews.create!({
+prod2.reviews.create!({
   user_id: 1,
   description: "It's actually less than meh.",
   rating: 2
