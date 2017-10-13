ce759a6767f8b63129c0718037aab163d0c77547
Matt Latimer <matt.latimer@gmail.com>
2017-10-13 12:57:22 -0700

    Added validations to Review model.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,8 @@ class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
 
+  validates :product_id, presence: true, numericality: {only_integer: true}
+  validates :user_id, presence: true, numericality: {only_integer: true}
+  validates :rating, presence: true, numericality: {only_integer: true}, inclusion: {in: (1..5)}
+
 end
