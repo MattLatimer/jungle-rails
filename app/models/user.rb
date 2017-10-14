fc6de6e16001467b105e1186d476e95b9fa934f4
Matt Latimer <matt.latimer@gmail.com>
2017-10-14 16:51:08 -0700

    Added validations to user and category models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
-
+  
   has_many :reviews
   
   has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 
 end
